811ee23a924d2320a4738acfcae397316c091c5a
carlosaenz26 <carlosaenz.26@hotmail.com>
2022-11-21 12:16:11 -0500

    cambio

--- FTP_viejo.py
+++ FTP_viejo.py
@@ -22,19 +22,19 @@ try:
     #print("Ubicado exitosamente en la carpeta: "+ ftp.pwd())
     #ftp.retrlines("LIST")
 
-    #print("Se ingresó correctamente al directorio")
+    print("Se ingresó correctamente al directorio")
 
     #os.chdir("C:\\Users\\user\\Desktop\\Proyecto Final") #Ruta para la descarga
 
-    archivo = input("Por favor ingrese el nombre del archivo que desea descargar: ")
-    ruta = input("Por favor ingrese la ruta de donde desee guardar el archivo: ")
-    os.chdir(ruta)
+    #archivo = input("Por favor ingrese el nombre del archivo que desea descargar: ")
+    #ruta = input("Por favor ingrese la ruta de donde desee guardar el archivo: ")
+    #os.chdir(ruta)
     #Una vez identificado el archivo a descargar usamos el comando reservado try/except
 
-    try:
+    #try:
         
-        with open(archivo, "wb") as f: #Abrimos el archivo con el formato WB, write bytes.
-            ftp.retrbinary("RETR " + archivo , f.write) #The method retrbinary() of the FTP class retrieves a file from the server to the local system in binary mode. 
+        #with open(archivo, "wb") as f: #Abrimos el archivo con el formato WB, write bytes.
+            #ftp.retrbinary("RETR " + archivo , f.write) #The method retrbinary() of the FTP class retrieves a file from the server to the local system in binary mode. 
                                                     #The method accepts a retrieval command such as RETR, and a callback function along with other paramaters.
                                                     #The method retrbinary() supports resumed FTP transfers. Using the parameter rest the position "1"
                                                     # from which the file transfer has to be made can be specified. "2"
@@ -42,11 +42,11 @@ try:
                                                     
 
             
-        print("Transferencia completa con un peso estimado de: ",ftp.size(archivo), "bytes")
+       # print("Transferencia completa con un peso estimado de: ",ftp.size(archivo), "bytes")
 
 
-    except ftplib.all_errors as e: #Manejo de excepciones
-        print("Errores: " +str(e))
+    #except ftplib.all_errors as e: #Manejo de excepciones
+        #print("Errores: " +str(e))
         
 except Exception as e: #Este except funciona, por ejemplo, en caso que las credenciales dadas sean incorrectas
     print("No se pudo iniciar sesión: "+str(e))
