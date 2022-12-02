11e2956008fa7ec647e780809c971a501879bbc1
Carlos Salomon Saenz Diaz <cs.saenz10@uniandes.edu.co>
2022-12-02 18:07:22 -0500

    todo

--- FTP_viejo.py
+++ FTP_viejo.py
@@ -1,57 +1,44 @@
 #%%
 from ftplib import FTP, all_errors
 import ftplib
-
-
 import os #Se ingresó este módulo por facilidad en la hora de descargar el archivo
+#%%
 
-
-ftp = FTP("10.0.2.15")
-usu1 = "cyberpunk"
-#input("Por favor ingrese su usuario: ") #Se pide el usuario
-pass1 = "cyberpunk2022"
-#input("Por favor ingrese su contraseña: ") #Se pide la clave
+ftp_server = FTP("157.253.192.111")
+usu1 = "pupi" #input("Por favor ingrese su usuario: ") #Se pide el usuario
+pass1 = "pupi" #input("Por favor ingrese su contraseña: ") #Se pide la clave
 
 try:
-    ftp.login(user=usu1,passwd=pass1) #Inicio de sesión FTP mediante las credenciales dadas anteriormente
+    ftp_server.login(user=usu1,passwd=pass1) #Inicio de sesión FTP mediante las credenciales dadas anteriormente
 
-    print(ftp.getwelcome()) #Mensaje de bienvenida
+    print(ftp_server.getwelcome()) #Mensaje de bienvenida
     print("Se inició sesión correctamente en el cliente FTP")
 
-    #ftp.cwd('imagenes_proyecto_final') #Se accede al directorio donde se encuentran los archivos a analizar
-    #print("Ubicado exitosamente en la carpeta: "+ ftp.pwd())
-    #ftp.retrlines("LIST")
+    ftp_server.cwd('home') #Se accede al directorio donde se encuentran los archivos a analizar
+    print("Ubicado exitosamente en la carpeta: "+ ftp_server.pwd())
+    ftp_server.retrlines("LIST")
 
     print("Se ingresó correctamente al directorio")
 
-    #os.chdir("C:\\Users\\user\\Desktop\\Proyecto Final") #Ruta para la descarga
-
-    #archivo = input("Por favor ingrese el nombre del archivo que desea descargar: ")
-    #ruta = input("Por favor ingrese la ruta de donde desee guardar el archivo: ")
-    #os.chdir(ruta)
-    #Una vez identificado el archivo a descargar usamos el comando reservado try/except
-
-    #try:
-        
-        #with open(archivo, "wb") as f: #Abrimos el archivo con el formato WB, write bytes.
-            #ftp.retrbinary("RETR " + archivo , f.write) #The method retrbinary() of the FTP class retrieves a file from the server to the local system in binary mode. 
-                                                    #The method accepts a retrieval command such as RETR, and a callback function along with other paramaters.
-                                                    #The method retrbinary() supports resumed FTP transfers. Using the parameter rest the position "1"
-                                                    # from which the file transfer has to be made can be specified. "2"
-                                                    
-                                                    
-
-            
-       # print("Transferencia completa con un peso estimado de: ",ftp.size(archivo), "bytes")
-
-
-    #except ftplib.all_errors as e: #Manejo de excepciones
-        #print("Errores: " +str(e))
+    # Enter File Name with Extension
+    filename = "650k.jpg"
+ 
+# Read file in binary mode
+    with open(filename, "wb") as file:
+    # Command for Uploading the file "STOR filename"
+        ftp_server.retrbinary(f"RETR {filename}", file.write)
+    # Display the content of downloaded file
+    file= open(filename, "r")
+    print('File Content:', file.read())
+    
+    # Close the Connection
+    ftp_server.quit()
+                               
         
 except Exception as e: #Este except funciona, por ejemplo, en caso que las credenciales dadas sean incorrectas
     print("No se pudo iniciar sesión: "+str(e))
     
 
-ftp.close()
+ftp_server.close()
 
 # %%
